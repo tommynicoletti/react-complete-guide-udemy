a899d3ebe5fa7b64394a70a8c0ab0d0a081d5932
tomnicol <Th3#Offspr1ng>
2022-03-12 23:48:55 +0100

    whao

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,15 +10,19 @@ const Expenses = (props) => {
     setFilteredYear(data);
   }
   const filteredExpenses = props.items.filter(expense=>expense.date.getFullYear().toString()===filteredYear);
+
+  let expensesContent = <p>No expenses found.</p>;
+  if(filteredExpenses.length>0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+    ));
+  }
+
   return (
     <div>
       <Card  className="expenses">
         <ExpensesFilter yearSelected={filteredYear}  onChangeYear={yearFilterHandler}  />
-        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-        {filteredExpenses.length > 0 && 
-          filteredExpenses.map((expense) => (
-            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-          ))}
+        {expensesContent}  
       </Card>
     </div>
   );
