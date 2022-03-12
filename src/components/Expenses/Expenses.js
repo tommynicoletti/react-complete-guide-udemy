dbc57ca4aaa6a3b2910bdbb5fe56d6c3a77254c9
tomnicol <Th3#Offspr1ng>
2022-03-12 23:44:47 +0100

    filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,20 +5,20 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState(2019);
+  const [filteredYear, setFilteredYear] = useState('2020');
   const yearFilterHandler = (data) =>{
-    console.log(data);
     setFilteredYear(data);
   }
+  const filteredExpenses = props.items.filter(expense=>expense.date.getFullYear().toString()===filteredYear);
   return (
     <div>
       <Card  className="expenses">
         <ExpensesFilter yearSelected={filteredYear}  onChangeYear={yearFilterHandler}  />
-        {props.items.map((expense, index) => {
-          return (
-            <ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date} />
-          );
-        })}
+        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 && 
+          filteredExpenses.map((expense) => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+          ))}
       </Card>
     </div>
   );
